c878bece24ab778b125031fd32195ad47644b676
Jan Wassermann <jan.wassermann@jansalleine.com>
2026-06-07 19:59:26 +0200

    allow tunes below $1000, down to $0400

--- test/test.sh
+++ test/test.sh
@@ -24,18 +24,16 @@ if [ $? -ne 0 ]; then echo "Test Phase 1 failed." && exit 1; fi
 ./sid2analyzer -nc -o prgs/We_Come_in_PETSCII_nc.prg sids/We_Come_in_PETSCII.sid
 if [ $? -ne 0 ]; then echo "Test Phase 1 failed." && exit 1; fi
 
-echo
-echo "Test Phase 2 – should fail with [ERROR]: music_start below 0x1000"
-echo "================================================================="
 ./sid2analyzer -o prgs/And_Now_for_Something_Different.prg sids/And_Now_for_Something_Different.sid
-if [ $? -ne 1 ]; then echo "Test Phase 2 failed." && exit 1; fi
-rm -f prgs/And_Now_for_Something_Different.prg
+if [ $? -ne 0 ]; then echo "Test Phase 1 failed." && exit 1; fi
+./sid2analyzer -nc -o prgs/And_Now_for_Something_Different_nc.prg sids/And_Now_for_Something_Different.sid
+if [ $? -ne 0 ]; then echo "Test Phase 1 failed." && exit 1; fi
 
 echo
-echo "Test Phase 3 – should fail with [ERROR]: sid_speed not 0x00000000"
+echo "Test Phase 2 – should fail with [ERROR]: sid_speed not 0x00000000"
 echo "================================================================="
 ./sid2analyzer -o prgs/25_Hertz-0_Inspiration.prg sids/25_Hertz-0_Inspiration.sid
-if [ $? -ne 1 ]; then echo "Test Phase 3 failed." && exit 1; fi
+if [ $? -ne 1 ]; then echo "Test Phase 2 failed." && exit 1; fi
 rm -f prgs/25_Hertz-0_Inspiration.prg
 
 rm -f sid2analyzer
